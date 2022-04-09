4b2ad00c23789bfbb1b5df6fb5ba4c52328ec3b4
Paweł Borowiec <pawel.2020.borowiec@gmail.com>
2022-04-09 19:14:39 +0200

    Deleted files due to structural problem

--- Conway/src/p1/MyButton.java
+++
@@ -1,9 +0,0 @@
-package p1;
-
-import javafx.scene.control.Button;
-
-public class MyButton extends Button {
-    int livingCellsCount=0;
-    Status status=Status.Untapped;
-
-}
--- Conway/src/p1/Status.java
+++
@@ -1,5 +0,0 @@
-package p1;
-
-public enum Status {
-    Tapped,Untapped;
-}
